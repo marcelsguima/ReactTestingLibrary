8283493773cef528697ea7d019cdbf35883431c1
Marcel <plasma@Marcel>
2022-09-19 15:58:48 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import App from '../App';
+
+describe('Testando o APP.js , verificar se', () => {
+  test('renderiza os links de navegação Home, About e Favorite Pokémons', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavPkmn = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(linkFavPkmn).toBeInTheDocument();
+  });
+});
