336432ce19626f0e2787157de41667537ba3a3cc
marcelsguima <marcelsguima@gmail.com>
2022-09-19 18:12:35 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { NotFound } from '../pages';
+
+describe('Testando o About.js , verificar se', () => {
+  test('se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+
+    const notFoundH2 = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundH2).toBeInTheDocument();
+  });
+
+  test('se a página contém a imagem do pikachu', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+
+    const imgPikachu = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(imgPikachu.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
