4b971b9e808a92af367b5bf61e6a11790b0ce9b8
marcelsguima <marcelsguima@gmail.com>
2022-09-19 18:01:37 -0300

    xRequisito 3-test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { FavoritePokemons } from '../pages';
+
+describe('Testando o FavoritePokemons.js , verificar se', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found'
+  + 'caso a pessoa não tenha pokémons favoritos;', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+
+    const favPkmn = screen.getByText(/no favorite pokemon found/i);
+    expect(favPkmn).toBeInTheDocument();
+  });
+});
